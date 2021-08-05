4053455a56e0f1d87d973228253114bfbb5a3627
Arnold Lee <arnoldlee60@gmail.com>
2021-08-05 16:13:59 -0500

    all nav items work except for dashboard logging out redirects to login

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,12 +2,39 @@ const router = require('express').Router();
 const { User } = require('../models');
 //const sequelize = require('../config/connection');
 //const { Post } = require('../models'); later
+const withAuth = require('../utils/auth'); //redirect to login if not logged in
 
-router.get('/',(req, res) => {
-  //console.log(req.session);
-  res.render('test')
+router.get('/', withAuth, async (req, res) => {
+//router.get('/',(req, res) => {
 
+    res.render('test')
   });
+
+  router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+
+  
+  router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('signup');
+  });
+
+  router.get('/logout', (req, res) => {
+    console.log("logged out")
+    //res.render('login')
+  });
+
+  
 
 
 module.exports = router;
