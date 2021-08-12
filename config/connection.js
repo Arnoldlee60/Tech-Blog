911c64b3f99efbe500c9fd5d789d65321227320d
Arnold Lee <arnoldlee60@gmail.com>
2021-08-12 15:31:10 -0500

    i messed up a lot in the hw now i have to kinda start over

--- config/connection.js
+++ config/connection.js
@@ -12,4 +12,22 @@ const sequelize = new Sequelize(
   }
 );
 
+// let sequelize;
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: 'localhost', 
+//       dialect: 'mysql', 
+//       port: 3306,
+//     },
+//   );
+// } //this is for heroku put variables in heroku jawsdb and use sql with those perameters
+
+
+
 module.exports = sequelize;
